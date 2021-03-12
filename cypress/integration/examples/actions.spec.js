02a34dc38fb8335ea39476854feec3bea36b4121
Kevin Horan <kevin.michael.horan@gmail.com>
2021-03-12 12:24:45 -0800

    Break tests

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -6,6 +6,10 @@ context('Actions', () => {
   })
 
   // https://on.cypress.io/interacting-with-elements
+  
+  it('breaks tests', () => {
+    expect(true).toBe(false);
+  });
 
   it('.type() - type into a DOM element', () => {
     // https://on.cypress.io/type
